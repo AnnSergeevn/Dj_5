14065befefc484cae1d5a4509046742f9bb572f4
Ann test <kolesnikova.88@mail.ru>
2024-04-24 23:32:49 +0400

    Correct_views.py

--- measurement/urls.py
+++ measurement/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from measurement.views import DemoView, AddMeasurement, SensorUpdateView, ListSensor
+from measurement.views import AddMeasurement, SensorUpdateView, ListSensor
 
 urlpatterns = [
     # TODO: зарегистрируйте необходимые маршруты
-    path('sensors/', DemoView.as_view()),
-    #path('sensors/<int:pk>', DemoView.as_view()),
+    path('sensors/', ListSensor.as_view()),
     path('sensors/<pk>/', SensorUpdateView.as_view()),
     path('measurements/', AddMeasurement.as_view()),
-    path('sensors/', ListSensor.as_view()),
 
 ]
--- measurement/views.py
+++ measurement/views.py
@@ -8,19 +8,6 @@ from django.http import HttpResponse
 from rest_framework.response import Response
 
 
-# def create_measurement(request):
-#     Measurement(temperature=22.3, created_at=datetime.datetime.now().time(), sensor = 1).save()
-#     return HttpResponse("Все получилось!")
-#
-#
-# def create_sensor(request):
-#     measurements = Measurement.objects.all()
-#     for measurement in measurements:
-#         Sensor.objects.create(name="ESP32", description="Перенес датчик на балкон", measurement=measurement)
-#     return HttpResponse("Все получилось!")
-#
-
-
 class AddMeasurement(CreateAPIView):
     """
         Ввод показаний датчика
@@ -29,28 +16,18 @@ class AddMeasurement(CreateAPIView):
     serializer_class = MeasurementSerializer
 
 
-class DemoView(ListCreateAPIView):
-    """
-            Информация по всем датчикам
-            """
-    queryset = Sensor.objects.all()
-    serializer_class = SensorDetailSerializer
-
 class ListSensor(ListCreateAPIView):
     """
-    Список всех датчиков и создание нового датчика
-    """
+            Список всех датчиков и создание нового датчика.
+            """
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
 
 
-class SensorView(CreateAPIView):
-
-    def post(self, request):
-        Measurement(temperature=35.3, created_at=datetime.datetime.now().time()).save()
-
-
 class SensorUpdateView(RetrieveUpdateAPIView):
+    """
+    Получение информации по датчику
+    """
     queryset = Sensor.objects.all()
     serializer_class = SensorDetailSerializer
 
